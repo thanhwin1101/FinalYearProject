5dea0b3b3fb3d55993527543309c772053a6c8a6
PHAN LE THANH NGUYEN <137856001+thanhwin1101@users.noreply.github.com>
2025-12-23 11:47:54 +0700

    Delete server directory

--- server/src/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  uid: { type: String, unique: true, required: true }, // UID thẻ RFID (in hoa)
-  name: { type: String, required: true },
-  email: { type: String }
-}, { timestamps: true });
-
-export default mongoose.model('User', userSchema);
